eb0de7dd48cc274e94a7bb52087c41c243d0a51b
Riku <rudnik7000@gmail.com>
2021-04-18 20:37:26 +0000

    Fixed CORS issues

--- src/api/router/router.go
+++ src/api/router/router.go
@@ -7,18 +7,19 @@ import (
 	"github.com/Riku32/waifu.pics/src/api/routes/info"
 	"github.com/Riku32/waifu.pics/src/api/routes/upload"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // New : initialize router
 func New(options api.Options) {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.DefaultCORSConfig))
 
 	api := e.Group("/api")
 	image.NewRouter(options, api)
 	admin.NewRouter(options, api)
 	upload.NewRouter(options, api)
 	info.NewRouter(options, api)
-	//fun.NewRouter(options, api)
 
 	api.GET("/endpoints", func(c echo.Context) error {
 		return c.JSON(200, options.Config.Endpoints)
